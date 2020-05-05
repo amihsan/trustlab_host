3263ea23cda10574b5e9c15fbbe0ee67a21288be
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2020-05-05 18:39:44 +0200

    refactored set_max_agents functionality out of register_at_director

--- connectors/basic_connector.py
+++ connectors/basic_connector.py
@@ -6,6 +6,10 @@ class BasicConnector(ABC):
     def register_at_director(self, max_agents):
         pass
 
+    @abstractmethod
+    def set_max_agents(self, max_agents):
+        pass
+
     def __init__(self, director_hostname):
         self.director_hostname = director_hostname
 
--- connectors/channels_connector.py
+++ connectors/channels_connector.py
@@ -7,6 +7,9 @@ class ChannelsConnector(BasicConnector):
     async def register_at_director(self, max_agents):
         uri = "ws://" + self.director_hostname + "/supervisors/"
         self.websocket = await websockets.client.connect(uri)
+        await self.set_max_agents(max_agents)
+
+    async def set_max_agents(self, max_agents):
         register_max_agents = {"type": "max_agents", "max_agents": max_agents}
         await self.websocket.send(json.dumps(register_max_agents))
         response = await self.websocket.recv()
