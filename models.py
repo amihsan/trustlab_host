05783452b9f55721fe0d53d169c5c16b5986cb9d
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2020-06-13 16:21:34 +0200

    changed observation as object

--- models.py
+++ models.py
@@ -1,6 +1,32 @@
 from serializer import Interface
 
 
+class Observation(Interface):
+    observation_id = int
+    before = list
+    sender = str
+    receiver = str
+    author = str
+    topic = str
+    message = str
+
+    def __init__(self, observation_id, before, sender, receiver, author, topic, message):
+        self.observation_id = observation_id
+        self.before = before
+        self.sender = sender
+        self.receiver = receiver
+        self.author = author
+        self.topic = topic
+        self.message = message
+        super().__init__(observation_id=observation_id, before=before, sender=sender, receiver=receiver, author=author,
+                         topic=topic, message=message)
+
+    def __eq__(self, other):
+        return self.observation_id == other.observation_id and self.before == other.before and \
+               self.sender == other.sender and self.receiver == other.receiver and self.author == other.author \
+               and self.topic == other.topic and self.message == other.message
+
+
 class Scenario(Interface):
     name = str
     agents = list
@@ -21,6 +47,12 @@ class Scenario(Interface):
             raise ValueError("Scenario agents must describe at least 2 agents.")
         if len(observations) == 0:
             raise ValueError("Scenario schedule must be not empty.")
+        for observation in observations:
+            if type(observation) != dict or not "observation_id" in observation.keys() or not \
+                    "sender" in observation.keys() or not "receiver" in observation.keys() or not \
+                    "author" in observation.keys() or not "topic" in observation.keys() or not \
+                    "message" in observation.keys() or not "before" in observation.keys():
+                raise ValueError("Each Observation requires to be dict with all its attributes.")
 
     def __init__(self, name, agents, observations, history, trust_thresholds, weights,
                  metrics_per_agent, authorities, topics, description="No one described this scenario so far."):
