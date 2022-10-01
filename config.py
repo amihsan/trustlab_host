caf7437faa0493acacd5fdfacdedb41cdd28d384
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2022-10-01 13:39:54 +0200

    adding WebSocket_Max for coming chunked websocket sending

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ LOG_PATH = PROJECT_PATH / 'log'
 # buffer size for TCP connections
 BUFFER_SIZE = 2048
 
+# determines the max size of one websocket message in bytes for potential chunked websocket messages
+WEBSOCKET_MAX = 900000  # tested this limit 990000 seems too large for supervisor side
+
 # aTLAS Version for check at supervisor <-> director connection
 ATLAS_VERSION = "v 0.0.1"
 
