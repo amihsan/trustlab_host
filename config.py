34077eda5405dc6d4638346c6f0d34dd2dcea941
Valentin Siegert <valentin.siegert@informatik.tu-chemnitz.de>
2022-10-01 16:57:54 +0200

    improving chunked transfer to both ends (director and supervisor) and enabling parallel chunked transfer with chunked transfer ids

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-from pathlib import Path
+from datetime import datetime
 from os.path import dirname, abspath
+from pathlib import Path
+from random import randint
 
 # path variables
 PROJECT_PATH = Path(abspath(dirname(__name__)))
@@ -15,3 +17,14 @@ WEBSOCKET_MAX = 900000  # tested this limit 990000 seems too large for superviso
 ATLAS_VERSION = "v 0.0.1"
 
 TIME_MEASURE = True
+
+
+def create_chunked_transfer_id():
+    """
+    Creates a new ID for chunked transfer within with datetime pattern plus 3 digit random int,
+    e.g. '2021-05-18_16-37-54_383'.
+
+    :return: new chunked transfer ID
+    :rtype: str
+    """
+    return f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{randint(0, 999):0=3d}'
